1722856287803dad9aaf2be53506a19fce071cce
bachacode <85910843+bachacode@users.noreply.github.com>
2023-06-07 17:48:35 -0400

    feat: filtros de carga academica

--- src/views/Auth/Reportes/CargasReporte.ts
+++ src/views/Auth/Reportes/CargasReporte.ts
@@ -28,6 +28,10 @@ export const cargas: Reportes = {
     }
   ],
   pdfName: 'carga_de_profesores.pdf',
+  filters: [
+    { value: 'nombre = null', name: 'Todos los profesores sin carga académica'},
+    { value: 'horas != null', name: 'Todos los profesores con carga académica'},
+  ],
   mapData: (record: ICargaTotal) => {
     if (record.horas == null) record.horas = 0
     record.horas_dadas = `${record.horas} / ${record.contrato_horas}`
