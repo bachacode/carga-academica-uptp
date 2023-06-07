0b1fd941cc79693dfb207135e20ca74fc73554fd
bachacode <85910843+bachacode@users.noreply.github.com>
2023-06-07 17:40:27 -0400

    feat: filtros de titulos academicos

--- src/views/Auth/Reportes/PosgradosReporte.ts
+++ src/views/Auth/Reportes/PosgradosReporte.ts
@@ -13,6 +13,12 @@ export const posgrados: Reportes = {
     }
   ],
   pdfName: 'posgrados.pdf',
+  filters: [
+    { value: 'grado ~ "maes%"', name: 'Posgrados de Maestría'},
+    { value: 'grado ~ "mag%"', name: 'Posgrados de Magíster'},
+    { value: 'grado ~ "doc%"', name: 'Posgrados de Doctorado'},
+    { value: 'grado !~ "1"', name: 'Todos los titulos de posgrado'},
+  ],
   mapData: (record: IPosgrado) => {
     return record
   }
