ee1fac12744568fa1432c6eedafb140b067e0129
bachacode <85910843+bachacode@users.noreply.github.com>
2023-06-11 10:29:58 -0400

    feat: graficas de titulos de pregrado y posgrado

--- src/views/Auth/Reportes/PosgradosReporte.ts
+++ src/views/Auth/Reportes/PosgradosReporte.ts
@@ -14,12 +14,40 @@ export const posgrados: Reportes = {
   ],
   pdfName: 'posgrados.pdf',
   filters: [
+    { value: '', name: 'Tablas', isLabel: true },
     { value: 'grado ~ "maes%"', name: 'Posgrados de Maestría' },
     { value: 'grado ~ "mag%"', name: 'Posgrados de Magíster' },
     { value: 'grado ~ "doc%"', name: 'Posgrados de Doctorado' },
-    { value: 'grado !~ "1"', name: 'Todos los titulos de posgrado' }
+    { value: 'grado !~ "1"', name: 'Todos los titulos de posgrado' },
+    { value: '', name: 'Graficas', isLabel: true },
+    { value: 'grado ~ "maes%@chart"', name: 'Posgrados de Maestría' },
+    { value: 'grado ~ "mag%"@chart', name: 'Posgrados de Magíster' },
+    { value: 'grado ~ "doc%"@chart', name: 'Posgrados de Doctorado' },
   ],
   mapData: (record: IPosgrado) => {
     return record
+  },
+  setChart: (values: IPosgrado[], allValues: number) => {
+    const totalFiltered = values.length
+    const grado = values[0].grado
+    return {
+      labels: ['Posgrados en el sistema Carga Académica'],
+      datasets: [
+        {
+          label: `Posgrados de ${grado}`,
+          data: [totalFiltered],
+          backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+          borderColor: ['rgba(255,99,132,1)'],
+          borderWidth: 1
+        },
+        {
+          label: 'Posgrados Totales',
+          data: [allValues],
+          backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+          borderColor: ['rgba(54, 162, 235, 1)'],
+          borderWidth: 1
+        }
+      ]
+    }
   }
 }
--- src/views/Auth/Reportes/TitulosReporte.ts
+++ src/views/Auth/Reportes/TitulosReporte.ts
@@ -14,12 +14,40 @@ export const titulos: Reportes = {
   ],
   pdfName: 'titulos.pdf',
   filters: [
+    { value: '', name: 'Tablas', isLabel: true },
     { value: 'grado ~ "t%"', name: 'Titulos Técnicos' },
     { value: 'grado ~ "i%"', name: 'Titulos de Ingeniería' },
     { value: 'grado ~ "l%"', name: 'Titulos de Licenciatura' },
-    { value: 'grado !~ "1"', name: 'Todos los titulos' }
+    { value: 'grado !~ "1"', name: 'Todos los titulos' },
+    { value: '', name: 'Graficas', isLabel: true },
+    { value: 'grado ~ "t%"@chart', name: 'Titulos Técnicos' },
+    { value: 'grado ~ "i%"@chart', name: 'Titulos de Ingeniería' },
+    { value: 'grado ~ "l%"@chart', name: 'Titulos de Licenciatura' },
   ],
   mapData: (record: ITitulo) => {
     return record
+  },
+  setChart: (values: ITitulo[], allValues: number) => {
+    const totalFiltered = values.length
+    const grado = values[0].grado
+    return {
+      labels: ['Titulos en el sistema Carga Académica'],
+      datasets: [
+        {
+          label: `Titulos de ${grado}`,
+          data: [totalFiltered],
+          backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+          borderColor: ['rgba(255,99,132,1)'],
+          borderWidth: 1
+        },
+        {
+          label: 'Titulos Totales',
+          data: [allValues],
+          backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+          borderColor: ['rgba(54, 162, 235, 1)'],
+          borderWidth: 1
+        }
+      ]
+    }
   }
 }
